88672165c05f15b653aab5315b18ab7b3fd1561f
Kimye <9964078+vinchona@users.noreply.github.com>
2022-12-25 18:16:23 +0100

    test: add client building

--- tests/client.rs
+++ tests/client.rs
@@ -1,7 +1,11 @@
 mod utils;
 
 #[test]
-fn tests() {
+fn client() {
     utils::setup();
+    let client = utils::client();
+    assert_eq!(client.id(), utils::TEST_CLIENT_ID);
+    assert_eq!(client.secret(), utils::TEST_CLIENT_SECRET);
+    assert_eq!(client.refresh_token(), utils::TEST_CLIENT_REFRESH_TOKEN);
     utils::teardown();
 }
--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,2 +1,17 @@
+use zohoxide_crm::Client;
+
+pub const TEST_CLIENT_ID: &str = "id";
+pub const TEST_CLIENT_SECRET: &str = "secret";
+pub const TEST_CLIENT_REFRESH_TOKEN: &str = "refresh_token";
+
 pub fn setup() {}
 pub fn teardown() {}
+
+/// Build a `Client`
+pub fn client() -> Client {
+    Client::builder()
+        .client_id(TEST_CLIENT_ID)
+        .client_secret(TEST_CLIENT_SECRET)
+        .refresh_token(TEST_CLIENT_REFRESH_TOKEN)
+        .build()
+}
